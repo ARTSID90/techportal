4db3f3e7224d7df3a5ecd239e16e7052c4182c23
ARTSID90 <sitdikov365@gmail.com>
2020-12-28 11:44:14 +0300

    1

--- portal/views.py
+++ portal/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def tech(request):
     return render(request, 'tech-index.html', {})
 
+
 def contact(request):
     return render(request, 'tech-contact.html', {})
