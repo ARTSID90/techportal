8c8667862579a176faf47f58c29df50f56d55fce
ARTSID90 <sitdikov365@gmail.com>
2021-02-12 20:28:42 +0300

    5

--- portal/forms.py
+++ portal/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    #recipient_list = forms.CharField(label='Ваше Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    #name = forms.CharField(label='Ваше Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
     #from_email = forms.EmailField(label='@', widget=forms.TextInput(attrs={'class': 'form-control'}))
     subject = forms.CharField(label='Ваше имя и Номер телефоа',  widget=forms.TextInput(attrs={'class': 'form-control'}))
     massage = forms.CharField(label='Текст', max_length=5000, widget=forms.Textarea({'class': 'form-control', "rows": 5}))
