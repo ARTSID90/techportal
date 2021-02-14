9b8919234e53fd3d3fb6c0c521f256438dc6faba
ARTSID90 <sitdikov365@gmail.com>
2021-02-14 21:45:33 +0300

    5

--- portal/urls.py
+++ portal/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('a', views.article1, name="article 1"),
     path('2', views.article2, name="article 2"),
     path('a51', views.articlea51, name="article a51"),
+    path('appoint', views.appointment, name="appointment"),
 ]
